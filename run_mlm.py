5c15f4d7f3ee587ed6d6612c2eac1ac70fc54f25
Enrique Noriega <enrique.noriega@gmail.com>
2022-06-23 19:01:43 -0700

    Bug fix

--- run_mlm.py
+++ run_mlm.py
@@ -448,12 +448,12 @@ def main():
 
     if data_args.max_seq_length is None:
         max_seq_length = tokenizer.model_max_length
-        if max_seq_length > 1024:
+        if max_seq_length > 512:
             logger.warning(
                 f"The tokenizer picked seems to have a very large `model_max_length` ({tokenizer.model_max_length}). "
                 "Picking 1024 instead. You can change that default value by passing --max_seq_length xxx."
             )
-            max_seq_length = 1024
+            max_seq_length = 512
     else:
         if data_args.max_seq_length > tokenizer.model_max_length:
             logger.warning(
@@ -462,107 +462,33 @@ def main():
             )
         max_seq_length = min(data_args.max_seq_length, tokenizer.model_max_length)
 
-    if data_args.line_by_line:
-        # When using line_by_line, we just tokenize each nonempty line.
-        padding = "max_length" if data_args.pad_to_max_length else False
+    # When using line_by_line, we just tokenize each nonempty line.
+    padding = "max_length" if data_args.pad_to_max_length else False
 
-        def tokenize_function(examples):
-            # Remove empty lines
-            examples[text_column_name] = [
-                line for line in examples[text_column_name] if len(line) > 0 and not line.isspace()
-            ]
-            return tokenizer(
-                examples[text_column_name],
-                padding=padding,
-                truncation=True,
-                max_length=max_seq_length,
-                # We use this option because DataCollatorForLanguageModeling (see below) is more efficient when it
-                # receives the `special_tokens_mask`.
-                return_special_tokens_mask=True,
-            )
+    def tokenize_function(examples):
+        # Remove empty lines
+        examples[text_column_name] = [
+            line for line in examples[text_column_name] if len(line) > 0 and not line.isspace()
+        ]
+        return tokenizer(
+            examples[text_column_name],
+            padding=padding,
+            truncation=True,
+            max_length=max_seq_length,
+            # We use this option because DataCollatorForLanguageModeling (see below) is more efficient when it
+            # receives the `special_tokens_mask`.
+            return_special_tokens_mask=True,
+        )
 
-        with training_args.main_process_first(desc="dataset map tokenization"):
-            tokenized_datasets = raw_datasets.map(
-                tokenize_function,
-                batched=True,
-                num_proc=data_args.preprocessing_num_workers,
-                remove_columns=[text_column_name, 'pmcid',  'keyword_rank'],
-                load_from_cache_file=not data_args.overwrite_cache,
-                desc="Running tokenizer on dataset line_by_line",
-            )
-    else:
-        # Otherwise, we tokenize every text, then concatenate them together before splitting them in smaller parts.
-        # We use `return_special_tokens_mask=True` because DataCollatorForLanguageModeling (see below) is more
-        # efficient when it receives the `special_tokens_mask`.
-        def tokenize_function(examples):
-            return tokenizer(examples[text_column_name], return_special_tokens_mask=True)
-
-        columns_to_remove = list(column_names)
-        columns_to_remove.remove('keyword_indices')
-
-        with training_args.main_process_first(desc="dataset map tokenization"):
-            eager_args = {
-                "num_proc": data_args.preprocessing_num_workers,
-                "load_from_cache_file": not data_args.overwrite_cache,
-                "desc": "Running tokenizer on every text in dataset",
-                "batched": True,
-            }
-
-            map_args = {
-                "remove_columns": columns_to_remove,
-            }
-
-            if not data_args.streaming:
-                map_args.update(**eager_args)
-
-
-            tokenized_datasets = raw_datasets.map(
-                tokenize_function,
-                **map_args
-            )
-
-        # Main data processing function that will concatenate all texts from our dataset and generate chunks of
-        # max_seq_length.
-        def group_texts(examples):
-            # Concatenate all texts.
-            concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
-            total_length = len(concatenated_examples[list(examples.keys())[0]])
-            # We drop the small remainder, we could add padding if the model supported it instead of this drop, you can
-            # customize this part to your needs.
-            if total_length >= max_seq_length:
-                total_length = (total_length // max_seq_length) * max_seq_length
-            # Split by chunks of max_len.
-            result = {
-                k: [t[i : i + max_seq_length] for i in range(0, total_length, max_seq_length)]
-                for k, t in concatenated_examples.items()
-            }
-            return result
-
-        # Note that with `batched=True`, this map processes 1,000 texts together, so group_texts throws away a
-        # remainder for each of those groups of 1,000 texts. You can adjust that batch_size here but a higher value
-        # might be slower to preprocess.
-        #
-        # To speed up this part, we use multiprocessing. See the documentation of the map method for more information:
-        # https://huggingface.co/docs/datasets/package_reference/main_classes.html#datasets.Dataset.map
-
-        # with training_args.main_process_first(desc="grouping texts together"):
-        #     eager_args = {
-        #         "num_proc": data_args.preprocessing_num_workers,
-        #         "load_from_cache_file": not data_args.overwrite_cache,
-        #         "desc": f"Grouping texts in chunks of {max_seq_length}",
-        #         "batched": True,
-        #     }
-        #
-        #     map_args = {
-        #
-        #     }
-        #
-        #     if not data_args.streaming:
-        #         map_args.update(**eager_args)
-        #     tokenized_datasets = tokenized_datasets.map(
-        #         group_texts,
-        #         **map_args
-        #     )
+    with training_args.main_process_first(desc="dataset map tokenization"):
+        tokenized_datasets = raw_datasets.map(
+            tokenize_function,
+            batched=True,
+            num_proc=data_args.preprocessing_num_workers,
+            remove_columns=[text_column_name, 'pmcid',  'keyword_rank'],
+            load_from_cache_file=not data_args.overwrite_cache,
+            desc="Running tokenizer on dataset line_by_line",
+        )
 
     if training_args.do_train:
         if "train" not in tokenized_datasets:
